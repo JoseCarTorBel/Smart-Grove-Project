df2a9ad622dad494d8cfab1890f1d9df515d8d17
XimoRaspberry <al375824@uji.es>
2020-12-05 18:23:17 +0100

    Actualizada ruta

--- Raspberry/arrancarServer.sh
+++ Raspberry/arrancarServer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e 'power on \nagent on\ndiscoverable on \npairable on \nquit' | bluetoothctl
-python3.7 /home/pi/Desktop/EI1057/Smart-grove-introductory-project/Raspberry/BluetoothServer.py 
+python3.7 /home/pi/Proyecto/Raspberry/BluetoothServer.py &
 
 
