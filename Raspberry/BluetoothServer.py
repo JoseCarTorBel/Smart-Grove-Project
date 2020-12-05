e022bd10d66baa7519baeca4a158564a1e33685e
XimoRaspberry <al375824@uji.es>
2020-12-05 19:13:46 +0100

    Añadido log con hora y control de excepcion particular

--- Raspberry/BluetoothServer.py
+++ Raspberry/BluetoothServer.py
@@ -1,6 +1,7 @@
 import socket
 import subprocess
 import logging
+from datetime import datetime
 
 
 def get_bt_mac():
@@ -9,7 +10,7 @@ def get_bt_mac():
         status, output = subprocess.getstatusoutput(cmd)
         bt_mac = output.split("{}:".format(device_id))[1].split("BD Address: ")[1].split(" ")[0].strip()
         #print("Raspberry> Mac: " + bt_mac.lower())
-        logging.info("Raspberry> Mac: " + bt_mac.lower())
+        logging.info(str(datetime.now())+" Raspberry> Mac: " + bt_mac.lower())
         return bt_mac.lower()
 
 
@@ -28,30 +29,31 @@ s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM
 s.bind((hostMACAddress,port))
 s.listen(backlog)
 while(1):
-		try:
-				#print("Raspberry> Espero conexion")
-				logging.info("Raspberry> Espero conexion")
-				client, address = s.accept()
-				#print("Raspberry> Conexion aceptada")
-				logging.info("Raspberry> Conexion aceptada")
-				while 1:
-						data = client.recv(size)
-						if data:
-								if(("killServer" in data.decode("utf-8"))):
-									client.close()
-									s.close()
-									exit()
-								logging.info("Raspberry> Recibido: "+data)
-								#print(data)
-								client.send(data)
-								
-		except KeyboardInterrupt:
+	try:
+		#print("Raspberry> Espero conexion")
+		logging.info(str(datetime.now())+" Raspberry> Espero conexion")
+		client, address = s.accept()
+		#print("Raspberry> Conexion aceptada")
+		logging.info(str(datetime.now())+" Raspberry> Conexion aceptada")
+		while 1:
+			data = client.recv(size)
+			if data:
+				if(("killServer" in data.decode("utf-8"))):
 					client.close()
 					s.close()
 					exit()
-		except: 
-				#print("Raspberry> Cierro conexion")
-				logging.info("Raspberry> Cierro conexion")
-				client.close()
-				#s.close()
-
+				logging.info(str(datetime.now())+" Raspberry> Recibido: "+data.decode("utf-8"))
+				#print(data)
+				client.send(data)
+					
+	except KeyboardInterrupt:
+		client.close()
+		s.close()
+		exit()
+	except ConnectionResetError: 
+		#print("Raspberry> Cierro conexion")
+		logging.info(str(datetime.now())+" Raspberry> Cierro conexion")
+		client.close()
+		#s.close()
+	except:
+		raise
