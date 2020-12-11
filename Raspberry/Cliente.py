040f5ed3ee42a65e7e5bf6d9a1f26fdd28c707a7
XimoRaspberry <al375824@uji.es>
2020-12-11 18:47:11 +0100

    Idem

---
+++ Raspberry/Cliente.py
@@ -0,0 +1,14 @@
+import socket
+
+serverMACAddress = 'F0:08:D1:C8:DB:FE'
+port = 1
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((serverMACAddress,port))
+while 1:
+    text = input()
+    if text == "quit":
+        break
+    s.send(bytes(text, 'UTF-8'))
+s.close()
+
+
