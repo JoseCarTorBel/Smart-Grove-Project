2527fe47e78e4cd754e788da2000ac576a6a3bfa
XimoRaspberry <al375824@uji.es>
2020-12-15 12:58:21 +0100

    Funcionando! Falta subir al drive cada 2 min

--- Raspberry/Cliente.py
+++ Raspberry/Cliente.py
@@ -4,8 +4,14 @@ import logging
 from datetime import datetime
 import I2C_LCD_driver
 import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from GoogleDrive import GoogleDrive
 
 lcd=I2C_LCD_driver.lcd()
+
+goodr = GoogleDrive()
+
 logging.basicConfig( level=logging.DEBUG, filename='/home/pi/SmartGrove/Raspberry/Cliente.log')
 
 serverMACAddress = 'F0:08:D1:C8:DB:FE'
@@ -18,19 +24,22 @@ while(1):
 		s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
 		logging.info("----------------------------------------------------")
+		lcd.lcd_display_strings(str(datetime.now()), "ESPERANDO AL CLIENTE")
 		logging.info(str(datetime.now())+" Raspberry> Intento conectarme al ESP32")
 		s.connect((serverMACAddress,port))
 		while 1:
 			data = s.recv(size)
 			if data:
-				strData = data.decode("utf-8")
-				logging.info(str(datetime.now())+" Raspberry> Recibido: "+strData)
-				lcd.lcd_display_strings(str(datetime.now()), strData)
-				s.send("ACK\n".encode("utf-8"))
+				strData = data.decode("UTF-8")
+				if('#' in strData):
+					logging.info(str(datetime.now())+" Raspberry> Recibido: "+strData)
+					lcd.lcd_display_strings(str(datetime.now()), str(strData))
+					#goodr.insertRow([strData])
+					s.send("ACK\n".encode("utf-8"))
 		s.close()
 	except KeyboardInterrupt:
 		logging.info(str(datetime.now())+" Raspberry> Cierro conexion (CTRL-C)")
-		lcd.lcd_display_strings(str(datetime.now()), "Servidor parado)")
+		lcd.lcd_display_strings(str(datetime.now()), "Cliente parado)")
 		s.close()
 		exit()
 	except ConnectionResetError: 
@@ -39,7 +48,10 @@ while(1):
 		s.close()
 		logging.info(str(datetime.now())+" Raspberry> Fallo")
 		time.sleep(5)
- 
+	except TimeoutError:
+		s.close()
+		logging.info(str(datetime.now())+" Raspberry> Fallo")
+		time.sleep(5) 
 	except:
 		raise
 
--- Raspberry/GoogleDrive.py
+++ Raspberry/GoogleDrive.py
@@ -1,34 +1,48 @@
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 
+class GoogleDrive:
 
-def getAllSheet(hoja):
-	return hoja.get_all_records()
+	scope=""
+	creds=""
+	client=""
+	sheet=""
 
-def getValue(hoja,fila,columna):
-	return hoja.cell(fila, columna).value
+	def __init__(self):
+		self.scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+		self.creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', self.scope)
+		self.client = gspread.authorize(self.creds)
+		self.sheet = self.client.open("ProyectoHuertos").sheet1
 
-def setValue(hoja,fila,columna,valor):
-	hoja.update_cell(fila, columna, valor)
+
+	def getAllSheet(self):
+		return self.sheet.get_all_records()
+
+	def getValue(self,fila,columna):
+		return self.sheet.cell(fila, columna).value
+
+	def setValue(self,fila,columna,valor):
+		self.sheet.update_cell(fila, columna, valor)
 	
-def setRow(hoja,fila,numFila):
-	hoja.insert_row(fila, numFila)
+	def setRow(self,fila,numFila):
+		self.sheet.insert_row(fila, numFila)
 	
-def getFirstRowEmpty(hoja):
-	i=1;
-	while(True):
-		if(getValue(hoja,i,1)==""):
-			return i;
-		i+=1
-def insert_row(hoja,fila):
-	setRow(hoja,fila,getFirstRowEmpty(hoja))
+	def getFirstRowEmpty(self):
+		i=1;
+		while(True):
+			if(self.getValue(i,1)==""):
+				return i;
+			i+=1
+	def insertRow(self,fila):
+		self.setRow(fila,self.getFirstRowEmpty())
 	
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+#scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+#creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+#client = gspread.authorize(creds)
 
-sheet = client.open("ProyectoHuertos").sheet1
-setValue(sheet,10,1,"Hola")
+#sheet = client.open("ProyectoHuertos").sheet1
+
 
