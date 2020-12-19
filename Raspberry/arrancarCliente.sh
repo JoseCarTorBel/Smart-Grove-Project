4de49f7ebd11c402881763aef34bd6afafea5dbb
iamximo <al375824@uji.es>
2020-12-19 12:08:53 +0100

    Documentacion y cambiadas rutas

--- Raspberry/arrancarCliente.sh
+++ Raspberry/arrancarCliente.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e 'power on \nagent on\ndiscoverable on \npairable on \nquit' | bluetoothctl
-python3.7 /home/pi/SmartGrove/Raspberry/BluetoothServer.py &
+python3.7 /home/pi/SmartGrove/Raspberry/Cliente.py &
 
 
